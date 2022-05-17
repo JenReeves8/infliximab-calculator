83be2966e7c8de1e4966a7be620222a631f68da7
JenniferFulton <jennifer.reeves8@gmail.com>
2022-05-17 13:25:00 -0400

    login app removed and routing updated

--- infusion_calculator/infusion_calculator/urls.py
+++ infusion_calculator/infusion_calculator/urls.py
@@ -17,6 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('login.urls')),
-    path('calc/', include('calculator.urls')),
+    path('', include('calculator.urls')),
 ]
