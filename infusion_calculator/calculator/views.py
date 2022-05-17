be0f668276edc86f6a64868099b252cbebacf5c6
JenniferFulton <jennifer.reeves8@gmail.com>
2022-05-17 13:34:03 -0400

    routing updated

--- infusion_calculator/calculator/views.py
+++ infusion_calculator/calculator/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render, redirect
 
 def home_page(request):
-    if 'user' not in request.session:
-        return redirect('/')
     return render(request, "home_page.html")
 
 def new_infusion(request):
-    if 'user' not in request.session:
-        return redirect('/')
     return render(request,"new_infusion.html")
